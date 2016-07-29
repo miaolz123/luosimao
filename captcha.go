4424c1a642759b055e2bb29abb7757372ff72c78
miaolz123 <miaolizhao@126.com>
2016-07-29 12:53:00 +0800

    fix make captcha bug

--- captcha.go
+++ captcha.go
@@ -2,16 +2,23 @@ package luosimao
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 // MakeCaptcha : make a captcha
 func MakeCaptcha(length int) string {
 	captcha := ""
-	for i := 0; i < length; i++ {
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		captcha += fmt.Sprint(r.Intn(10))
+	if length < 1 {
+		return captcha
 	}
-	return captcha
+	max, _ := strconv.ParseInt(fmt.Sprintf("%.f", math.Pow10(length)), 10, 0)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	captcha = fmt.Sprint(r.Int63n(max))
+	for len(captcha) < length {
+		captcha = "0" + captcha
+	}
+	return captcha[0:length]
 }
